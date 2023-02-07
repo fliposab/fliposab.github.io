79123cf13efcf00afd9ad11a2d1c58480ed8caec
fliposab <102372402+fliposab@users.noreply.github.com>
2023-02-07 18:36:31 +0100

    deleted useless files

--- images/hideshow.js
+++
@@ -1,8 +0,0 @@
-function openDescription(){
-    var x = document.getElementById("Sdesc");
-    if(x.style.display == "block"){
-        x.style.display = "none";
-    } else{
-        x.style.display="block";
-    }
-}
