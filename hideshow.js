ace5ee4735e4bd8d322d2976ad580cd79c731784
fliposab <102372402+fliposab@users.noreply.github.com>
2023-02-07 18:18:57 +0100

    Upload site

---
+++ hideshow.js
@@ -0,0 +1,8 @@
+function openDescription(){
+    var x = document.getElementById("Sdesc");
+    if(x.style.display == "block"){
+        x.style.display = "none";
+    } else{
+        x.style.display="block";
+    }
+}
